7a7ceaada01158e3dec110d1440c3a2d34c959fd
Vikas Malik <113085489+Desi385@users.noreply.github.com>
2025-01-23 13:56:57 +0530

    Update questions.py

--- config/questions.py
+++ config/questions.py
@@ -1,8 +1,8 @@
 '''
-Author:     Sai Vignesh Golla
-LinkedIn:   https://www.linkedin.com/in/saivigneshgolla/
+Author:     Vikas Malik
+LinkedIn:   https://www.linkedin.com/in/vikas-malik-3a854310/
 
-Copyright (C) 2024 Sai Vignesh Golla
+Copyright (C) 2024 Vikas Malik
 
 License:    GNU Affero General Public License
             https://www.gnu.org/licenses/agpl-3.0.en.html
@@ -22,27 +22,27 @@ version:    24.12.29.12.30
 default_resume_path = "all resumes/default/resume.pdf"      # (In Development)
 
 # What do you want to answer for questions that ask about years of experience you have, this is different from current_experience? 
-years_of_experience = "5"          # A number in quotes Eg: "0","1","2","3","4", etc.
+years_of_experience = "20"          # A number in quotes Eg: "0","1","2","3","4", etc.
 
 # Do you need visa sponsorship now or in future?
 require_visa = "No"               # "Yes" or "No"
 
 # What is the link to your portfolio website, leave it empty as "", if you want to leave this question unanswered
-website = "https://github.com/GodsScion"                        # "www.example.bio" or "" and so on....
+website = ""                        # "www.example.bio" or "" and so on....
 
 # Please provide the link to your LinkedIn profile.
-linkedIn = "https://www.linkedin.com/in/saivigneshgolla/"       # "https://www.linkedin.com/in/example" or "" and so on...
+linkedIn = "https://www.linkedin.com/in/vikas-malik-3a854310/"       # "https://www.linkedin.com/in/example" or "" and so on...
 
 # What is the status of your citizenship? # If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
 # Valid options are: "U.S. Citizen/Permanent Resident", "Non-citizen allowed to work for any employer", "Non-citizen allowed to work for current employer", "Non-citizen seeking work authorization", "Canadian Citizen/Permanent Resident" or "Other"
-us_citizenship = "U.S. Citizen/Permanent Resident"
+us_citizenship = "Non-citizen allowed to work for current employer"
 
 
 
 ## SOME ANNOYING QUESTIONS BY COMPANIES 🫠 ##
 
 # What to enter in your desired salary question (American and European), What is your expected CTC (South Asian and others)?, only enter in numbers as some companies only allow numbers,
-desired_salary = 1200000          # 80000, 90000, 100000 or 120000 and so on... Do NOT use quotes
+desired_salary = 1000000          # 80000, 90000, 100000 or 120000 and so on... Do NOT use quotes
 '''
 Note: If question has the word "lakhs" in it (Example: What is your expected CTC in lakhs), 
 then it will add '.' before last 5 digits and answer. Examples: 
@@ -54,7 +54,7 @@ And if asked in months, then it will divide by 12 and answer. Examples:
 '''
 
 # What is your current CTC? Some companies make it compulsory to be answered in numbers...
-current_ctc = 800000            # 800000, 900000, 1000000 or 1200000 and so on... Do NOT use quotes
+current_ctc = 85000            # 800000, 900000, 1000000 or 1200000 and so on... Do NOT use quotes
 '''
 Note: If question has the word "lakhs" in it (Example: What is your current CTC in lakhs), 
 then it will add '.' before last 5 digits and answer. Examples: 
@@ -66,7 +66,7 @@ then it will add '.' before last 5 digits and answer. Examples:
 '''
 
 # (In Development) # Currency of salaries you mentioned. Companies that allow string inputs will add this tag to the end of numbers. Eg: 
-# currency = "INR"                 # "USD", "INR", "EUR", etc.
+# currency = "USD"                 # "USD", "INR", "EUR", etc.
 
 # What is your notice period in days?
 notice_period = 30                   # Any number >= 0 without quotes. Eg: 0, 7, 15, 30, 45, etc.
@@ -82,12 +82,12 @@ then it will divide by 30 or 7 and answer respectively. Examples:
 '''
 
 # Your LinkedIn headline in quotes Eg: "Software Engineer @ Google, Masters in Computer Science", "Recent Grad Student @ MIT, Computer Science"
-linkedin_headline = "Full Stack Developer with Masters in Computer Science and 4+ years of experience" # "Headline" or "" to leave this question unanswered
+linkedin_headline = "20+ year multi faceted manager in variety of Data engineering and Data Science Experience" # "Headline" or "" to leave this question unanswered
 
 # Your summary in quotes, use \n to add line breaks if using single quotes "Summary".You can skip \n if using triple quotes """Summary"""
 linkedin_summary = """
-I'm a Senior Software Engineer at Amazon with Masters in CS and 4+ years of experience in developing and maintaining Full Stack Web applications and cloud solutions. 
-Specialized in React, Node.js, and Python.
+Dynamic career of 20+ years that reflects rich Software Development
+and Project Management experience in Data Science and Data Engineering across e-Commerce, Automotive, FMCG & Supply Chain Management domains
 """
 
 '''
@@ -96,7 +96,18 @@ Note: If left empty as "", the tool will not answer the question. However, note 
 
 # Your cover letter in quotes, use \n to add line breaks if using single quotes "Cover Letter".You can skip \n if using triple quotes """Cover Letter""" (This question makes sense though)
 cover_letter = """
-Cover Letter
+Dear Sir,
+I am excited to apply for the Data Analyst position at Sherwin Williams. With a proven background in data analysis, visualization, and actionable insights generation, coupled with 20 yrs of experience working in data-driven roles, I am eager to contribute my expertise to support your mission of leveraging data for impactful decision-making.
+In my previous role(s) at WIPRO, I successfully developed an automated dashboard that reduced reporting time by 30% and was able to reduce 5 redundant jobs. My experience includes analyzing large datasets, ensuring data integrity, and creating intuitive visualizations that guide strategic planning. I am proficient in tools like SQL, Python, Tableau/BI, and Excel, and I have experience working with cloud technologies such as Google Big Query and AWS to manage and analyze data effectively.
+What excites me most about the opportunity at Williams is working for products which most of the industries and humans are consuming. My ability to combine technical expertise with strategic thinking aligns perfectly with your mission.
+Some highlights of my qualifications include:
+•	Data Analysis and Interpretation: Expertise in deriving actionable insights from complex datasets, supporting leadership in making informed decisions.
+•	Advanced Reporting Skills: Skilled in creating automated, interactive dashboards using BI/ Node technologies, enabling real-time monitoring and improved communication.
+•	Problem-Solving Mindset: Adept at identifying data anomalies, ensuring accuracy, and providing solutions for process improvement.
+Beyond technical skills, I take pride in my ability to collaborate with cross-functional teams, communicate findings effectively, and adapt to the evolving demands of data analytics. I am confident that my skills and passion for data will make me an asset to your team.
+I would welcome the opportunity to discuss how my background, skills, and enthusiasm can contribute to the success of Sherwin Williams. Thank you for considering my application. I look forward to the possibility of contributing to your team and am available at your earliest convenience for an interview.
+Sincerely,
+Vikas Malik
 """
 
 '''
@@ -104,7 +115,7 @@ Note: If left empty as "", the tool will not answer the question. However, note 
 ''' 
 
 # Name of your most recent employer
-recent_employer = "Not Applicable" # "", "Lala Company", "Google", "Snowflake", "Databricks"
+recent_employer = "Dispute Resolution Center" # "", "Lala Company", "Google", "Snowflake", "Databricks"
 
 # Example question: "On a scale of 1-10 how much experience do you have building web or mobile applications? 1 being very little or only in school, 10 being that you have built and launched applications to real users"
 confidence_level = "8"             # Any number between "1" to "10" including 1 and 10, put it in quotes ""
@@ -151,6 +162,6 @@ As an independent developer, I pour my heart and soul into creating tools like t
 Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
 
 Gratefully yours 🙏🏻,
-Sai Vignesh Golla
+Vikas Malik
 '''
 ############################################################################################################
